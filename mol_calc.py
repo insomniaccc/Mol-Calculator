da1a35638a468fdba5bade87ab1ca0bdf6c5a41f
insomniaccc <34196040+insomniaccc@users.noreply.github.com>
2017-12-13 22:58:48 -0600

    Update mol_calc.py

--- mol_calc.py
+++ mol_calc.py
@@ -5,52 +5,44 @@
 
 av_num = 6.02 * 10**23
 
-print "Please enter everything exactly as you see it. Remember, use '**' to indicate power-to, ex 10^23 = 10**23." \
+print("Please enter everything exactly as you see it. Remember, use 'e+#' to indicate power to. Ex. 10^23 = e+23.")
 
-prob_type = raw_input("What are you trying to find? (mol amount, mass, things...)")
+prob_type = input("What are you trying to find? (mol amount, mass, things...)")
 
 if prob_type == "things":
-    thng_init = raw_input("What do you know? (grams, mol amount)")
+    thng_init = input("What do you know? (grams, mol amount)")
     if thng_init == "grams":
-        thng_init_grams = input("Enter mass.")
-        thng_init_molarmass = input("Enter element or molecules molar mass.")
-        mol_and_mass = thng_init_grams * av_num
-        final_thng_grams = mol_and_mass / thng_init_molarmass
-        print final_thng_grams
+        thng_init_grams = float(input("Enter mass."))
+        thng_init_molarmass = float(input("Enter element or molecules molar mass."))
+        print((thng_init_grams * av_num) / thng_init_molarmass)
 
 
     if thng_init == "mol amount":
-        thng_init_mol = input("Enter mol amount.")
-        mol_and_molarmass = thng_init_mol * av_num
-        print mol_and_molarmass
+        thng_init_mol = float(input("Enter mol amount."))
+        print(thng_init_mol * av_num)
 
 
 if prob_type == "mass":
-    mass_init = raw_input("What do you know? (things, mol amount)")
+    mass_init = input("What do you know? (things, mol amount)")
     if mass_init == "things":
-        mass_init_thng = input("Enter number of things.")
-        mass_init_molarmass = input("Enter molar mass.")
-        thng_molarmass = mass_init_thng * mass_init_molarmass
-        final_mass_thng = thng_molarmass / av_num
-        print final_mass_thng
+        mass_init_thng = float(input("Enter number of things."))
+        mass_init_molarmass = float(input("Enter molar mass."))
+        print((mass_init_thng * mass_init_molarmass) / av_num)
 
     if mass_init == "mol amount":
-        mass_init_mol = input("Enter mol amount.")
-        mass_init_molarmass_molamnt = input("Enter molar mass.")
-        final_mass_mol = mass_init_mol * mass_init_molarmass_molamnt
-        print final_mass_mol
+        mass_init_mol = float(input("Enter mol amount."))
+        mass_init_molarmass_molamnt = float(input("Enter molar mass."))
+        print(mass_init_mol * mass_init_molarmass_molamnt)
 
 
 
 if prob_type == "mol amount":
-    mol_init = raw_input("What do you know? (mass, things)")
+    mol_init = input("What do you know? (mass, things)")
     if mol_init == "mass":
-        mol_init_mass = input("Enter the mass.")
-        mol_init_molarmass = input("Enter molar mass.")
-        final_mol_mass = mol_and_mass / mol_and_molarmass
-        print final_mol_mass
+        mol_init_mass = float(input("Enter the mass."))
+        mol_init_molarmass = float(input("Enter molar mass."))
+        print(mol_init_mass / mol_init_molarmass)
 
     if mol_init == "things":
-        mol_init_thng = input("Enter the number of things.")
-        final_mol_thng = mol_init_thng / av_num
-        print final_mol_thng
+        mol_init_thng = float(input("Enter the number of things."))
+        print(mol_init_thng / av_num)
